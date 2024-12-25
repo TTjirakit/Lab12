2658c4b54f34fbcb5a63bdeb62f30895e15c8aa4
TTjirakit <captain112006@gmail.com>
2024-12-25 21:47:36 +0700

    lab11_2 completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+long long int gcd(long long int x , long long y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long long int gcd(long long int x , long long y){
+    if(x % y == 0){
+        return y;
+    }else{
+        y = gcd(y, x%y);
+        return y;
+    }
 }
